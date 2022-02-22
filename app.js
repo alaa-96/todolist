338580964c1d761f23723d530233615535e0c511
Alaa Atef <alaa00x0@gmail.com>
2022-02-22 07:53:50 +0200

    files

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const listItems = ['ex. Buy coffee'];
+const workItems = [];
+const date = require(__dirname + '/date.js');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  const day = date.getDate();
+
+  res.render('list', { listTitle: day, newListItems: listItems });
+});
+
+app.post('/', function (req, res) {
+  const listItem = req.body.newItem;
+  if (req.body.list === 'Work List ') {
+    workItems.push(listItem);
+    res.redirect('/work');
+  } else {
+    listItems.push(listItem);
+    res.redirect('/');
+  }
+});
+
+app.get('/work', function (req, res) {
+  res.render('list', { listTitle: `Work List`, newListItems: workItems });
+});
+
+app.post('/work', function (req, res) {
+  const listItem = req.body.newItem;
+  workItems.push(listItem);
+  res.redirect('/work');
+});
+
+app.get('/about', function (req, res) {
+  res.render('about');
+});
+
+app.listen(3000, function () {
+  console.log('Server started on port 3000');
+});
